85822c24f8de5e52f6c33033c8587c59869f73e4
dell <dell@DESKTOP-J81OTQD>
2022-09-28 16:45:23 +0530

    Modified

---
+++ src/test/java/onlineShoppingApp/CartTest.java
@@ -0,0 +1,20 @@
+package onlineShoppingApp;
+
+import org.testng.annotations.Test;
+
+public class CartTest {
+
+	@Test
+	public void cartPage()
+	{
+		System.out.println("---Books--");
+		
+	}
+	
+	@Test
+	public void cartFashion()
+	{
+		System.out.println("---Shirt--");
+		
+	}
+}
